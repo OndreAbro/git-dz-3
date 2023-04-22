8f63a13d2ee752a0d34e4a0c3934434ad9de7c93
OndreAbro <denbrysommemanen@gmail.com>
2023-04-22 18:23:43 +0700

    fix task 26

--- task_26.py
+++ task_26.py
@@ -2,9 +2,12 @@
 # и возводит число А в целую степень B с помощью рекурсии.
 
 def rec_exponention(a, b):
-    if b > 1:
-        a *= rec_exponention(a, b - 1)
-    return a
+    if b == 0:
+        return 1
+    else:
+        if abs(b) > 1:
+            a *= rec_exponention(a, abs(b) - 1)
+        return a if b > 0 else 1 / a
 
 
 try:
